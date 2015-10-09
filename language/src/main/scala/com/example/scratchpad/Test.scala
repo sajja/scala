fadbbe939fe95decb12949ac402301a6acbba434
Sajith Silva <sajiths@pagero.com>
2015-10-09 19:16:39 +0530

    State experiment

--- language/src/main/scala/com/example/scratchpad/Test.scala
+++ language/src/main/scala/com/example/scratchpad/Test.scala
@@ -1,52 +1,12 @@
 package com.example.scratchpad
 
-import com.example.scratchpad.Crypto.cryptOp
-
-import scala.sys.process.Process
-import scala.util.Try
-
-
-object Crypto {
-  type cryptOp = (Array[Byte], Map[String, Key]) => Try[Array[Byte]]
-}
-
-class Key(val key: String) {
-  override def toString: String = key
-}
-
-object Key {
-
-  def apply(key: String) = new Key(key)
-}
-
-object Encrypt {
-  def apply[A](algo: String): cryptOp = {
-    if (algo == "RSA") (payload: Array[Byte], settings: Map[String, Key]) => {
-      Try {
-        val x = "Encrypting " + new String(payload) + " with algo " + algo + " with settings " + settings.mkString(",")
-        println(x)
-        ("encrypted "  + new String(payload)).getBytes
-      }
-    } else { (payload: Array[Byte], setting: Map[String, Key]) => Try("".getBytes()) }
-  }
-}
-
-object Decrypt {
-  def apply[A](algo: String): cryptOp = {
-    if (algo == "RSA") (payload: Array[Byte], settings: Map[String, Key]) => {
-      Try {
-        val x = "Decrypting "  + new String(payload) + " with algo " + algo + " with settings " + settings.mkString(",")
-        println(x)
-        ("decrypted "  + new String(payload)).getBytes
-      }
-    } else { (payload: Array[Byte], setting: Map[String, Key]) => Try("".getBytes()) }
-  }
-}
 
 object Test {
+  def add(x:Int)=x+42
   def main(args: Array[String]): Unit = {
-    val x = List(Map("1"->1,"2"->2,"3"->3),Map("1"->1,"2"->2,"3"->3),Map("1"->1,"2"->2,"3"->3))
-    println(for (i<-x) yield i.foldLeft(" ")((s: String, tuple: (String, Int)) => s + tuple._1 + tuple._2))
+    val i:Option[Int]  = Some(10)
+    val j = i.map(add)
+    println(j.get)
   }
 }
 
--- language/src/main/scala/com/example/state/StateExperiment.scala
+++ language/src/main/scala/com/example/state/StateExperiment.scala
@@ -1,28 +1,69 @@
 package com.example.state
 
-class State()
+import scala.util.Random
 
-case class HealthyState() extends State
+class State(val currentHitPoints: Int, val baseHitPoints: Int)
 
-case class DeadState() extends State
+case class Healthy(current: Int, base: Int) extends State(current, base)
 
-case class BlodiedState() extends State
+case class Dead(base: Int) extends State(0, base)
 
-class Warrior(val hp: Int, val ac: Int, val state: State)
+case class Blodied(current: Int, base: Int) extends State(current, base)
 
-object StateTest {
-  def transform2[A, B](prevStaet: A)(f: A => B) = {
-    f(prevStaet)
+object Event {
+  def apply(f: State => State)(current: State) = {
+    f(current)
+  }
+}
+
+object Events {
+  def posInt: Int = {
+    val i = Random.nextInt(10)
+    if (i > 0) i
+    else posInt
   }
 
-  def printState(state: State) = println(state)
+  type event = State => State
 
-  def attack(warrior: Warrior, damage: Int) = {
-   
+  def apply(et: String): event = {
+    et match {
+      case "damage" => (f: State) => f match {
+        case state: Dead =>
+          println("You cannot damage dead unit")
+          state
+        case state: Healthy =>
+          val hp = posInt
+          println("Dealing some damage " + hp)
+          val newHp = state.currentHitPoints - hp
+          if (newHp <= 0) new Dead(state.base)
+          else if (newHp * 2 < state.currentHitPoints) Blodied(newHp, state.base)
+          else new Healthy(newHp, state.base)
+      }
+      case "heal" => (f: State) => f match {
+        case state: Dead =>
+          println("You cannot heal a dead unit")
+          state
+        case state: Healthy =>
+          val hp = posInt
+          println("Healing some damage " + hp)
+          val newHp = state.currentHitPoints + hp
+          if (newHp * 2 < state.baseHitPoints) Blodied(newHp, state.baseHitPoints)
+          else if (newHp >= state.baseHitPoints) new Healthy(newHp, state.baseHitPoints)
+          else new Healthy(newHp, state.baseHitPoints)
+      }
+    }
   }
+}
 
+object Test {
   def main(args: Array[String]): Unit = {
-    val p1 = new Warrior(100, 10, new HealthyState)
+    val damage = Events("damage")
+    val heal = Events("heal")
 
+    println(damage(new Healthy(10, 10)))
+    println(heal(new Dead(10)))
+    println(heal(new Healthy(1, 20)))
+    println(heal(new Healthy(10, 20)))
+    println(heal(new Healthy(10, 20)))
   }
 }
