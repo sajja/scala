ea661eb705a434133d4a04cb843e59ed8c6f52ba
Sajith Silva <sajiths@pagero.com>
2016-06-24 21:13:49 +0530

    Combining two monads

--- language/src/main/scala/com/example/monad/experiment/Basic.scala
+++ language/src/main/scala/com/example/monad/experiment/Basic.scala
@@ -1,8 +1,39 @@
 package com.example.monad.experiment
 
-/**
-  * Created by sajith on 4/27/16.
-  */
-class Basic {
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
+
+case class FutureO[+A](future: Future[Option[A]]) extends AnyVal {
+  def flatMap[B](f: A => FutureO[B])(implicit ec: ExecutionContext): FutureO[B] = {
+    val newFuture = future.flatMap {
+      case Some(a) => f(a).future
+      case None => Future.successful(None)
+    }
+    FutureO(newFuture)
+  }
+
+  def map[B](f: A => B)(implicit ec: ExecutionContext): FutureO[B] = {
+    FutureO(future.map(option => option map f))
+  }
+}
+
+object TestBasic {
+  def findUser(name: String): Future[Option[User]] = Future(Some(User(name)))
+
+
+  case class User(name: String)
+
+  case class Address(name: String)
+
+  def findAddress(userName: String): Future[Option[Address]] = Future(Some(Address(s"$userName's Address")))
+
+  def main(args: Array[String]): Unit = {
+    val z:Future[Option[Address]] = (for {
+      a <- FutureO(findUser("xxx"))
+      b <- FutureO(findAddress(a.name))
+    } yield b).future
+
+    println(z)
+  }
 }
